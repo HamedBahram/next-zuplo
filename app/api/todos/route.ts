7bea076928f5f6bc0788190fda30d4836d80684c
Hamed Bahram <hamed.bahram@gmail.com>
2023-11-24 10:50:00 -0500

    Add secret key to todos endpoint

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,9 +1,17 @@
-import { getTodos } from '@/lib/fake-db'
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { getTodos } from '@/lib/fake-db'
 
 export const runtime = 'edge'
 
 export async function GET() {
+  // Only allow requests from your Gateway
+  const headersList = headers()
+  const secret = headersList.get('secret-key')
+  if (secret !== process.env.SECRET_KEY) {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+  }
+
   // Faking a database call
   const data = await getTodos()
 
